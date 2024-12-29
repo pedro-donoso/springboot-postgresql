6f708c45e60223a8924ad8ee71bbb2b2f5d052cf
pedro-donoso <pedro.donoso.r@usach.cl>
2024-12-29 13:38:37 -0300

    creo ProductController

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -1,14 +1,8 @@
 package com.example.crud;
 
-import com.example.crud.product.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -16,18 +10,5 @@ public class CrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
-	}
-
-	@GetMapping(path = "/")
-	public List<Product> getNames(){
-			return  List.of(
-			 new Product(
-					2541L,
-					"Laptop",
-                     500F,
-					LocalDate.of(2025, Month.MARCH,5),
-					2
-			)
-		);
 	}
 }
---
+++ src/main/java/com/example/crud/product/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.crud.product;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/products")
+public class ProductController {
+
+    @GetMapping
+    public List<Product> getProducts(){
+        return  List.of(
+                new Product(
+                        2541L,
+                        "Laptop",
+                        500F,
+                        LocalDate.of(2025, Month.MARCH,5),
+                        2
+                )
+        );
+    }
+
+}
