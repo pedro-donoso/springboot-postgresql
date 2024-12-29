8d32158e4a2a9f9af40a55321b8c6cfa1a78e543
pedro-donoso <pedro.donoso.r@usach.cl>
2024-12-29 12:39:54 -0300

    muestro nombres json

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -2,12 +2,28 @@ package com.example.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class CrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
+	}
+
+	@GetMapping(path = "/")
+	public List<String> getNames(){
+		return List.of(
+				"Chris",
+				"Josh",
+				"Jeff",
+				"Jane",
+				"Jenny"
+		);
 	}
 
 }
