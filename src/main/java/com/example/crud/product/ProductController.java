f195cc699ff97deef9e5037bb6da79bdb178fd05
pedro-donoso <pedro.donoso.r@usach.cl>
2024-12-29 13:47:41 -0300

    inyeccion de dependencias

--- src/main/java/com/example/crud/product/ProductController.java
+++ src/main/java/com/example/crud/product/ProductController.java
@@ -1,27 +1,25 @@
 package com.example.crud.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/products")
 public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping
     public List<Product> getProducts(){
-        return  List.of(
-                new Product(
-                        2541L,
-                        "Laptop",
-                        500F,
-                        LocalDate.of(2025, Month.MARCH,5),
-                        2
-                )
-        );
+        return this.productService.getProducts();
     }
+
 
 }
---
+++ src/main/java/com/example/crud/product/ProductService.java
@@ -0,0 +1,22 @@
+package com.example.crud.product;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class ProductService {
+    public List<Product> getProducts(){
+        return  List.of(
+                new Product(
+                        2541L,
+                        "Laptop",
+                        500F,
+                        LocalDate.of(2025, Month.MARCH,5),
+                        2
+                )
+        );
+    }
+}
